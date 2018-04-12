a7638400fe71c40989e4cb0a86fc8c9b042c6a52
eliuzhc <>
2018-04-12 19:20:08 +0200

    inizio esercitazione

---
+++ src/simulatore_so/Processo.java
@@ -0,0 +1,15 @@
+package simulatore_so;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author eliuzhc
+ */
+public class Processo extends Thread{
+    
+}
